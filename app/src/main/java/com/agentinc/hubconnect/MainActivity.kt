2b734348bfec29c8adc7687782c42bf6b60f2851
Basil <balasaravanan_dhanwin_basil@s2023.ssts.edu.sg>
2026-03-09 16:03:58 +0800

    haha

--- app/src/main/java/com/agentinc/hubconnect/MainActivity.kt
+++ app/src/main/java/com/agentinc/hubconnect/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text()
+    Text("test")
 }
 
 @Preview(showBackground = true)
