318739e35fb75294d2e7cd8df85b6df42abc0588
Basil <balasaravanan_dhanwin_basil@s2023.ssts.edu.sg>
2026-03-09 16:01:46 +0800

    removed text

---
+++ app/src/main/java/com/agentinc/hubconnect/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.agentinc.hubconnect
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.agentinc.hubconnect.ui.theme.HubconnectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HubconnectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HubconnectTheme {
+        Greeting("Android")
+    }
+}
